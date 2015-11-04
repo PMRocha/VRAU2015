1bf4450219d62a0c11a75b0d7e758881642a8bf3
Hugo Freixo <hugo1245@gmail.com>
2015-11-04 10:31:04 +0000

    Bots look at each other

---
+++ Teste_RVAU/Assets/PlayerLookAt.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerLookAt : MonoBehaviour
+{
+    public Transform target;
+
+    void Update()
+    {
+        if (target.GetComponent<MeshRenderer>().isVisible)
+        {
+            transform.LookAt(target);
+           // transform.Rotate(new Vector3(0, -90, 0));
+        }
+    }
+}
