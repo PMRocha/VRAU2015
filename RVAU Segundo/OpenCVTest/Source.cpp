5acc2717f85950aa61e14502f5041ec9f9817149
Hugo Freixo <hugo1245@gmail.com>
2015-12-16 20:46:25 +0000

    If the cards are the same the algorithm gets the card right. Can't run with cards.png. An error occurs.

--- RVAU Segundo/OpenCVTest/Source.cpp
+++ RVAU Segundo/OpenCVTest/Source.cpp
@@ -14,6 +14,10 @@ bool compareContourAreas(std::vector<Point> contour1, std::vector<Point> contour
 	return (i > j);
 }
 
+bool sortByX(pair<Mat,int> vec1, pair<Mat, int> vec2) {
+	return (vec1.second < vec2.second);
+}
+
 Mat preprocess(Mat img) {
 	cvtColor(img, img, CV_BGR2GRAY);
 	GaussianBlur(img, img, Size(1, 1), 1000);
@@ -21,15 +25,51 @@ Mat preprocess(Mat img) {
 
 	return img;
 }
-Mat cardDiff(Mat img1, Mat img2) {
-	Mat output;
-	resize(img1, img1, img2.size());
-	absdiff(preprocess(img1), preprocess(img2), output);
-	GaussianBlur(output, output, Size(5, 5), 5);
-	threshold(output, output, 200, 255, THRESH_BINARY);
-	return output;
+vector<double> compareIMG(Mat img1, Mat img2) {
+	Mat hsv1, hsv2;
+
+	cvtColor(img1, hsv1, CV_BGR2HSV);
+	cvtColor(img2, hsv2, CV_BGR2HSV);
+
+	int h_bins = 50; int s_bins = 60;
+	int histSize[] = { h_bins, s_bins };
+
+	float h_ranges[] = { 0, 180 };
+	float s_ranges[] = { 0, 256 };
+
+	const float* ranges[] = { h_ranges, s_ranges };
+
+	int channels[] = { 0, 1 };
+
+	MatND hist1, hist2;
+
+	calcHist(&hsv1, 1, channels, Mat(), hist1, 2, histSize, ranges, true, false);
+	normalize(hist1, hist1, 0, 1, NORM_MINMAX, -1, Mat());
+
+	calcHist(&hsv2, 1, channels, Mat(), hist2, 2, histSize, ranges, true, false);
+	normalize(hist1, hist1, 0, 1, NORM_MINMAX, -1, Mat());
+	vector<double> vec;
+	for (int i = 0; i < 4; i++)
+	{
+		int compare_method = i;
+		vec.push_back(compareHist(hist1, hist2, compare_method));
+	}
+	return vec;
 }
 vector<Mat> readCards() {
+	string const suit[4] = { "copas","espadas","ouros","paus" };
+	vector<Mat> vec;
+	for (int i = 0; i < 4; i++) {
+			string str = "C:\\Users\\Asus\\Documents\\VRAU2015\\RVAU Segundo\\x64\\Release\\cards\\naipes\\"
+				+ suit[i] + ".png";
+			Mat image = imread(str, CV_LOAD_IMAGE_COLOR);
+
+			vec.push_back(image);
+	}
+	
+	return vec;
+}
+vector<Mat> readAllCards() {
 	string const suit[4] = { "copas","espadas","ouros","paus" };
 	vector<Mat> vec;
 	for (int i = 0; i < 4; i++) {
@@ -41,9 +81,10 @@ vector<Mat> readCards() {
 			vec.push_back(image);
 		}
 	}
-	
+
 	return vec;
 }
+
 
 int main( int argc, char** argv )
 {
@@ -86,8 +127,6 @@ int main( int argc, char** argv )
 		double peri = arcLength(card, true);
 		approxPolyDP(card, approximate, 0.01*peri, true);
 		RotatedRect rect = minAreaRect(card);
-		Mat r;
-		boxPoints(rect, r);
 
 		inputQuad[0] = approximate[0];
 		inputQuad[1] = approximate[1];
@@ -109,52 +148,77 @@ int main( int argc, char** argv )
 	//-------------------------------Get Symbols----------------------------------//
 	
 
-	thresh = preprocess(cards[2]);
+	thresh = preprocess(cards[1]);
 	findContours(thresh, vecPoint, vecInfo, CV_RETR_TREE, CHAIN_APPROX_SIMPLE);
 	std::sort(vecPoint.begin(), vecPoint.end(), compareContourAreas);
+	vector<pair<Mat,int>> symbols;
 
 	for (int i = 0; i < 10; ++i) {
-
-		//Scalar color = Scalar(0, 0,255);
-		//drawContours(cards[2], vecPoint, i, color, 2, 8, vecInfo, 0, Point());
-
+		
 		vector<Point> symbol = vecPoint[i];
 		double peri = arcLength(symbol, true);
+
 		approxPolyDP(symbol, approximate, 0.01*peri, true);
 		RotatedRect rect = minAreaRect(symbol);
-		Mat croppedFaceImage = cards[2](rect.boundingRect()).clone();
+		Mat r;
+		boxPoints(rect, r);
+	
+		rectangle(cards[1], rect.boundingRect(), Scalar(0, 0, 255));
 
+		
+		Mat croppedFaceImage = cards[1](rect.boundingRect()).clone();
+		symbols.push_back(pair<Mat, int>(croppedFaceImage, rect.center.x));
+	}	
+	std::sort(symbols.begin(), symbols.end(), sortByX);
+	//---------------------------------------------------------------------------------//
 
-		namedWindow("Display window" + to_string(i), WINDOW_AUTOSIZE);// Create a window for display.
-		imshow("Display window" + to_string(i), croppedFaceImage);
+	vector<Mat> allCards = readAllCards(); //getDataSet
 
-	}
-	namedWindow("Display window", WINDOW_AUTOSIZE);// Create a window for display.
-	imshow("Display window", cards[2]);
-	/*
-	vector<Mat> allCards = readCards();
-	int ZeroPixels = -1;
-	int best = -1;
+	//Compare symbols with Dataset
+/*
+	double lastResult = -1;
+	int card, symbol;
+	
 	for (int i = 0; i < allCards.size(); ++i) {
-		Mat card = cardDiff(cards[2], allCards[i]);
-		int TotalNumberOfPixels = card.rows * card.cols;
-		if (ZeroPixels < TotalNumberOfPixels - countNonZero(card)) {
-			ZeroPixels = TotalNumberOfPixels - countNonZero(card);
-			best = i;
+		vector<double> v1 = compareIMG(symbols[0].first, allCards[i]);
+		vector<double> v2 = compareIMG(symbols[1].first, allCards[i]);
+
+		double result = (v1[0] * v1[2]) / (v1[1] * v1[3]);
+		if (result > lastResult) {
+			lastResult = result;
+			card = i;
+			symbol = 0;
+		}
+		result = (v2[0] * v2[2]) / (v2[1] * v2[3]);
+		if (result > lastResult) {
+			lastResult = result;
+			card = i;
+			symbol = 1;
 		}
 	}
 
-	namedWindow("Display window1", WINDOW_AUTOSIZE);// Create a window for display.
-	imshow("Display window1", cardDiff(cards[2], allCards[best]));
 
-	namedWindow("Display window2", WINDOW_AUTOSIZE);// Create a window for display.
-	imshow("Display window2", cards[2]);
-
-	namedWindow("Display window3", WINDOW_AUTOSIZE);// Create a window for display.
-	imshow("Display window3", allCards[best]);
-
-	cout << "final step\n";
+	cout << lastResult << " " << card << " " << symbol << endl;
 	*/
+	double lastResult = -1;
+	int card;
+	for (int i = 0; i < allCards.size(); ++i) {
+		vector<double> v = compareIMG(cards[1], allCards[i]);
+		double result = (v[0] * v[2]) / (v[1] * v[3]);
+		if (result > lastResult) {
+			lastResult = result;
+			card = i;
+		}
+	}
+
+
+	namedWindow("Display window1", WINDOW_AUTOSIZE);// Create a window for display.
+	imshow("Display window1", cards[1]);
+	//namedWindow("Display window2", WINDOW_AUTOSIZE);// Create a window for display.
+	//imshow("Display window2", symbols[symbol].first);
+	namedWindow("Display window3", WINDOW_AUTOSIZE);// Create a window for display.
+	imshow("Display window3", allCards[card]);
+	
 	waitKey(0);                                          // Wait for a keystroke in the window
     return 0;
 }
